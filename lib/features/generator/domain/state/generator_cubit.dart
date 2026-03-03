0aeccc537be5cb800a0170c8c5dfb7a20968e564
Sandro Abashidze <123871213+Iamyouandwhatiseeisme@users.noreply.github.com>
2026-03-03 23:32:18 +0400

    feat(generator): implement GeneratorState for managing color states and update GeneratorCubit methods

--- lib/features/generator/domain/state/generator_cubit.dart
+++ lib/features/generator/domain/state/generator_cubit.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:random_color_generator/features/generator/data/interfaces/generator_interface.dart';
+import 'package:random_color_generator/features/generator/domain/state/generator_state.dart';
 
 /// This is a Cubit class for managing the state of the color generator feature.
 /// It extends [Cubit] with a state of type [Color]
-class GeneratorCubit extends Cubit<Color> {
+class GeneratorCubit extends Cubit<GeneratorState> {
   /// Generator service that generates a random color
   final GeneratorInterface generatorService;
 
   /// Constructor receives an instance of[GeneratorInterface]
   /// and creates an initial state with a default color (black).
-  GeneratorCubit({required this.generatorService}) : super(Colors.black);
+  GeneratorCubit({required this.generatorService})
+    : super(
+        GeneratorState(
+          backgroundColor: LinearGradient(
+            colors: [
+              generatorService.generateColor(),
+              generatorService.generateColor(),
+            ],
+          ),
+          buttonBackgroundColor: generatorService.generateColor(),
+        ),
+      );
 
   /// A method used to generate random color from the generator page
-  void generateRandomColor() {
-    final randomColor = generatorService.generateColor();
-    emit(randomColor);
+  Future<void> generateRandomColor() async {
+    final backGroundColorGradientTop = generatorService.generateColor();
+    final backgroundColorGradientBottom = generatorService.generateColor();
+    final buttonBackgroundColor = generatorService.generateColor();
+    emit(
+      state.copyWith(
+        backgroundColor: LinearGradient(
+          begin: AlignmentGeometry.topLeft,
+          end: AlignmentGeometry.bottomRight,
+          colors: [backGroundColorGradientTop, backgroundColorGradientBottom],
+        ),
+        buttonBackgroundColor: buttonBackgroundColor,
+      ),
+    );
+  }
+
+  /// A method used to realign the gradients of the background color
+  void realignGradients() {
+    final randomGradientDirection1 = generatorService.generateRandomDirection();
+    final randomGradientDirection2 = generatorService.generateRandomDirection();
+    emit(
+      state.copyWith(
+        backgroundColor: LinearGradient(
+          begin: randomGradientDirection1,
+          end: randomGradientDirection2,
+          colors: [
+            state.backgroundColor.colors.first,
+            state.backgroundColor.colors.last,
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/generator/domain/state/generator_state.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+/// State object for Generator cubit
+class GeneratorState {
+  /// This is the background color that will be used in the generator page
+  final LinearGradient backgroundColor;
+
+  /// This is the text color that will be used in the generator page, it should
+  final Color buttonBackgroundColor;
+
+  /// Constructor for GeneratorState, requires backgroundColor and textColor,
+  /// and an optional loading map that defaults to
+  /// a map with [GeneratorStateEvent]
+  GeneratorState({
+    required this.backgroundColor,
+    required this.buttonBackgroundColor,
+  });
+
+  /// This is a copyWith method for [GeneratorState] for emitting
+  /// new states without creating new instances
+  GeneratorState copyWith({
+    LinearGradient? backgroundColor,
+    Color? buttonBackgroundColor,
+  }) {
+    return GeneratorState(
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      buttonBackgroundColor:
+          buttonBackgroundColor ?? this.buttonBackgroundColor,
+    );
+  }
+}
