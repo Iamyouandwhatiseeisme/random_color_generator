60174fe3d39266c4708bd234ec1ce8a53b3ad5b0
Sandro Abashidze <123871213+Iamyouandwhatiseeisme@users.noreply.github.com>
2026-03-03 23:32:13 +0400

    fix(generator): update generateRandomNumber method signature and improve random direction generation

--- lib/features/generator/data/interfaces/generator_interface.dart
+++ lib/features/generator/data/interfaces/generator_interface.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 /// This is an interface for the generator feature, defining the contract for
 /// generating color
@@ -7,7 +7,7 @@ abstract class GeneratorInterface {
   Color generateColor();
 
   /// Generates a random number and returns it as an integer.
-  int generateRandomNumber(int seed);
+  int generateRandomNumber({int? customCeiling});
 
   /// Generates a list of RGBA values and returns it as a list of integers.
   List<int> generateValues({List<int>? colors = const []});
@@ -15,4 +15,8 @@ abstract class GeneratorInterface {
   /// Generates a color from a list of RGBA values
   /// and returns it as a Color object
   Color generateColorFromList(List<int> colors);
+
+  /// Generates a random direction for the gradient and returns it as an
+  /// [AlignmentGeometry] object.
+  AlignmentGeometry generateRandomDirection();
 }
--- lib/features/generator/domain/implementations/generator_service.dart
+++ lib/features/generator/domain/implementations/generator_service.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
-import 'dart:ui';
 
+import 'package:flutter/material.dart';
 import 'package:random_color_generator/consts/consts.dart';
 import 'package:random_color_generator/features/generator/data/interfaces/generator_interface.dart';
 
@@ -13,17 +13,15 @@ class GeneratorService implements GeneratorInterface {
   }
 
   @override
-  int generateRandomNumber(int seed) {
-    return Random().nextInt(ceiling);
+  int generateRandomNumber({int? customCeiling}) {
+    return Random().nextInt(customCeiling ?? rgbValueCeiling);
   }
 
   /// This is a recursive method that generates a list of ints for color values.
   @override
   List<int> generateValues({List<int>? colors = const []}) {
     final colorsList = [...?colors];
-    final randomColor = generateRandomNumber(
-      DateTime.now().millisecondsSinceEpoch,
-    );
+    final randomColor = generateRandomNumber();
     if (colorsList.length == 4) return colorsList;
     colorsList.add(randomColor);
 
@@ -34,5 +32,25 @@ class GeneratorService implements GeneratorInterface {
   @override
   Color generateColorFromList(List<int> colors) {
     return Color.fromARGB(colors.first, colors[1], colors[2], colors[3]);
+  }
+
+  @override
+  AlignmentGeometry generateRandomDirection() {
+    final list = [
+      AlignmentGeometry.topLeft,
+      AlignmentGeometry.topRight,
+      AlignmentGeometry.bottomLeft,
+      AlignmentGeometry.bottomRight,
+      AlignmentGeometry.center,
+      AlignmentGeometry.centerLeft,
+      AlignmentGeometry.centerRight,
+      AlignmentGeometry.topCenter,
+      AlignmentGeometry.bottomCenter,
+      AlignmentGeometry.centerStart,
+      AlignmentGeometry.centerEnd,
+    ];
+    return list[generateRandomNumber(
+      customCeiling: list.length,
+    )];
   }
 }
