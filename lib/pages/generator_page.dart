babe872923a0675cc456ccbb96043d7f8fede178
Sandro Abashidze <123871213+Iamyouandwhatiseeisme@users.noreply.github.com>
2026-03-03 20:44:47 +0400

    feat(app): implement GeneratorPage and update main app structure

---
+++ lib/pages/generator_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+/// This is the main page for random color generator App.
+class GeneratorPage extends StatelessWidget {
+  /// Creates an instance of [GeneratorPage].
+  const GeneratorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        debugPrint('Tapped on the screen');
+      },
+      child: const Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'Hello there',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
